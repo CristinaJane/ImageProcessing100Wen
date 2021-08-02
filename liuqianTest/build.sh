f8cc1bd4211c44a7c42f0301bc0f57ad8b7283dd
bessliuqian <bessliuqian@bessliuqiandeMacBook-Pro.local>
2021-08-02 22:45:43 +0800

    update

---
+++ liuqianTest/build.sh
@@ -0,0 +1,1 @@
+g++ $(pkg-config --cflags --libs opencv) -std=c++11 main.cpp -o test
--- liuqianTest/main.cpp
+++ liuqianTest/main.cpp
@@ -1,4 +1,12 @@
 //
 // Created by bessliuqian on 2021/8/1.
 //
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    int i = 10;
+    std::cout << "i=" << i << std::endl;
+    return 0;
+}
